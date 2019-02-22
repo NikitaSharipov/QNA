09c42abf7d421be4cf93a71afe7c2bd0bcd8544f
NikitaSharipov <sharipovkzn@mail.ru>
2019-02-22 10:21:31 +0300

    12 universal method

--- app/controllers/oauth_callbacks_controller.rb
+++ app/controllers/oauth_callbacks_controller.rb
@@ -1,45 +1,37 @@
 class OauthCallbacksController < Devise::OmniauthCallbacksController
 
   def github
-
-    #    if request.env['omniauth.auth']
-    #      if  request.env['omniauth.auth']['info']
-    #        email = request.env['omniauth.auth']['info']['email']
-    #      end
-    #    end
-
-    @user = User.find_for_oauth(request.env['omniauth.auth'], request.env['omniauth.auth'].try(:[], 'info').try(:[], 'email'))
-
-    if @user&.persisted?
-      sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: 'Github') if is_navigational_format?
-    else
-      redirect_to root_path, alert: 'Something went wrong'
-    end
+    connection('Github', request.env['omniauth.auth'].try(:[], 'info').try(:[], 'email'))
   end
 
   def facebook
-    connection('Facebook')
-  end
-
-  def connection(provider_name)
-    if User.where(facebook_name: request.env['omniauth.auth'].try(:[], 'info').try(:[], 'name')).length == 0
-      if request.env['omniauth.auth'].try(:[], 'info').try(:[], 'email').blank? && !session[:pre_email]
+    if User.where(facebook_name: request_info.try(:[], 'name')).length == 0
+      if request_info.try(:[], 'email').blank? && !session[:pre_email]
         redirect_to new_advanced_registration_path and return
       end
     end
 
-    email = session[:pre_email] || User.where(facebook_name: request.env['omniauth.auth'].try(:[], 'info').try(:[], 'name')).first&.email
+    email = session[:pre_email] || User.where(facebook_name: request_info.try(:[], 'name')).first&.email
 
+    connection('Facebook', email)
+  end
+
+  def connection(provider_name, email)
     @user = User.find_for_oauth(request.env['omniauth.auth'], email)
 
     if @user&.persisted?
-      @user.add_facebook_name(request.env['omniauth.auth'].try(:[], 'info').try(:[], 'email'))
+      @user.add_facebook_name(request_info.try(:[], 'email'))
       sign_in_and_redirect @user, event: :authentication
       set_flash_message(:notice, :success, kind: provider_name) if is_navigational_format?
     else
       redirect_to root_path, alert: 'Something went wrong'
     end
+  end
+
+  private
+
+  def request_info
+    request.env['omniauth.auth'].try(:[], 'info')
   end
 
 end
