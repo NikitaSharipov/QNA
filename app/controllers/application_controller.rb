27ae3d2bfbe3b41299a42b7581691e263f7e5c57
NikitaSharipov <sharipovkzn@mail.ru>
2019-02-27 15:30:07 +0300

    13

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
 
   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_url, alert: exception.message
+
+    respond_to do |format|
+      format.html { redirect_to root_path, alert: exception.message }
+      format.js { render json: exception.message, status: :forbidden }
+      format.json { render json: exception.message, status: :forbidden }
+    end
+
   end
 
   check_authorization unless: :devise_controller?
