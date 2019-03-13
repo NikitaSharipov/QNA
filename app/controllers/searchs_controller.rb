6c73d57551cb302a399716067a5f5b3af50fd863
NikitaSharipov <sharipovkzn@mail.ru>
2019-03-13 16:14:12 +0300

    16 refactoring

--- app/controllers/searchs_controller.rb
+++ app/controllers/searchs_controller.rb
@@ -3,17 +3,7 @@ class SearchsController < ApplicationController
   skip_authorization_check
 
   def index
-    @result = category.search(params[:search], page: params[:page], per_page: 3)
-  end
-
-  private
-
-  def category
-    if params[:category] == 'Global'
-      ThinkingSphinx
-    else
-      Object.const_get(params[:category]) if Object.const_get(params[:category])
-    end
+    @result = Services::SearchSphinxService.new.search(params[:category], params[:search], params[:page])
   end
 
 end
--- spec/controllers/searchs_controller_spec.rb
+++ spec/controllers/searchs_controller_spec.rb
@@ -3,11 +3,24 @@ require 'rails_helper'
 RSpec.describe SearchsController, type: :controller do
   describe 'GET #index' do
     context 'if valid search query' do
+      let(:sphinx_service) { double(Services::SearchSphinxService) }
+
+      before do
+        allow(Services::SearchSphinxService).to receive(:new).and_return(sphinx_service)
+      end
+
+      it 'searches for result' do
+        expect(sphinx_service).to receive(:search).with('Global', 'test', '1')
+        get :index, params: {category: 'Global', search: 'test', page: '1'}
+      end
+
       it "should render :index" do
+        allow(sphinx_service).to receive(:search).with('Global', 'test', '1').and_return([])
         get :index, params: {category: 'Global', search: 'test', page: '1'}
 
         expect(response).to render_template(:index)
       end
+
     end
   end
 end
