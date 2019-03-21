6651b1de5519ac9f36402e14583880b3bae4ac4b
NikitaSharipov <sharipovkzn@mail.ru>
2019-03-21 12:24:43 +0300

    config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => 'http://178.62.241.234/'}
+  config.action_mailer.default_url_options = { :host => '178.62.241.234'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     user_name:      ENV['SENDMAIL_USERNAME'],
