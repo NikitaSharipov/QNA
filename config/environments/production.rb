edf43009a0debf3245415399acfb2f0c6f3f4c4b
NikitaSharipov <sharipovkzn@mail.ru>
2019-03-21 14:14:34 +0300

    raise delivery errors false

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { :host => '178.62.241.234'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
