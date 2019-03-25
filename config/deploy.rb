2579df62f3a91c8ca064f28229a68eae1be20b43
NikitaSharipov <sharipovkzn@mail.ru>
2019-03-25 16:02:14 +0300

    sphinx

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :deploy_to, "/home/deployer/qna"
 set :deploy_user, 'deployer'
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", 'config/master.key', 'config/application.yml'
+append :linked_files, "config/database.yml", 'config/master.key', 'config/application.yml', 'config/thinking_sphinx.yml'
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", 'storage'
